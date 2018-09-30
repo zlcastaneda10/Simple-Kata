0cfc2d125544029a5a46dc0fe5806794b27106b5
Zulma Castañeda <zl.castaneda10@uniandes.edu.co>
2018-09-29 21:49:21 -0500

    Paso 1 - Test para un elemento en cadena (Rojo)

--- test_estadistica.py
+++ test_estadistica.py
@@ -4,3 +4,6 @@ from Estadistica import Estadistica
 class TestEstadistica(TestCase):
     def test_dar_estadistica(self):
         self.assertEqual(Estadistica().dar_estadistica(""),[0,0,0,0],"Cadena vacia")
+
+    def test_dar_estadistica_unacadena(self):
+        self.assertEqual(Estadistica().dar_estadistica("1"),[1,0,0,0],"Cadena vacia")
