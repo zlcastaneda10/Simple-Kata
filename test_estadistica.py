05a7250bdea598ae40fcf0fdc9a77e8877ed3f31
Cristian Martinez <tura182@gmail.com>
2018-09-29 23:21:38 -0500

    Paso 11- Se crea prueba para calcular promedio con cadena vacia (Verde)

--- test_estadistica.py
+++ test_estadistica.py
@@ -38,3 +38,6 @@ class TestEstadistica(TestCase):
     def test_dar_estadistica_maximo_nnumeros(self):
         self.assertEqual(Estadistica().dar_estadistica("1,2,3,4"), [4, 1, 4, 0], "maximo N numeros")
 
+    def test_dar_estadistica_promedio_vacio(self):
+        self.assertEqual(Estadistica().dar_estadistica(""), [0, 0, 0, 0], "cadena vacia")
+
