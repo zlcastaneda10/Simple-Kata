ee1de92223781dfc88b8d30b7283339cf45cd55e
Zulma Castañeda <zl.castaneda10@uniandes.edu.co>
2018-09-29 21:55:54 -0500

    Paso 3 - Test para dos elementos en cadena (Rojo)

--- test_estadistica.py
+++ test_estadistica.py
@@ -6,4 +6,7 @@ class TestEstadistica(TestCase):
         self.assertEqual(Estadistica().dar_estadistica(""),[0,0,0,0],"Cadena vacia")
 
     def test_dar_estadistica_unacadena(self):
-        self.assertEqual(Estadistica().dar_estadistica("1"),[1,0,0,0],"Cadena vacia")
+        self.assertEqual(Estadistica().dar_estadistica("1"),[1,0,0,0],"Un numero")
+
+    def test_dar_estadistica_doscadenas(self):
+        self.assertEqual(Estadistica().dar_estadistica("1,2"), [2, 0, 0, 0], "dos numeros")
